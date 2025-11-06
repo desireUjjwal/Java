cf798fb153ba1bda315e94277e2ccf7e81f302e7
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-06 16:38:45 +0530

    Add Demo1_Variables.java to demonstrate variable usage

---
+++ Core Java/Demo1_Variables.java
@@ -0,0 +1,8 @@
+public class Demo1_Variables {
+    public static void main(String[] args) {
+        int num1 = 10; // Integer variable
+        int num2 = 20; // Integer variable
+        int sum = num1 + num2; // Sum of num1 and num2
+        System.out.println("Sum: " + sum);
+    }
+}
