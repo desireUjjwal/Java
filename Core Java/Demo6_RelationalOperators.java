5d890f3f5d02f7f4aed3fdc45a464b79a0cb5a81
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-06 19:20:19 +0530

    Add Demo6_RelationalOperators.java for comparison demo

---
+++ Core Java/Demo6_RelationalOperators.java
@@ -0,0 +1,13 @@
+public class Demo6_RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+        System.out.println("a > b: " + (a > b));   // false
+        System.out.println("a < b: " + (a < b));   // true
+        System.out.println("a >= b: " + (a >= b)); // false
+        System.out.println("a <= b: " + (a <= b)); // true
+    }
+}
