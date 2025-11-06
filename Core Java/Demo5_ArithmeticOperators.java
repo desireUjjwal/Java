6d7e04a3f8b4655883c68912addb0b89227b36e7
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-06 19:17:30 +0530

    Add Demo5_ArithmeticOperators.java for basic ops

---
+++ Core Java/Demo5_ArithmeticOperators.java
@@ -0,0 +1,15 @@
+public class Demo5_ArithmaticOperators {
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 4;
+        System.out.println("Addition: " + (num1 + num2)); // 19
+        System.out.println("Subtraction: " + (num1 - num2)); // 11
+        System.out.println("Multiplication: " + (num1 * num2)); // 60
+        System.out.println("Division: " + (num1 / num2)); // 3  
+        System.out.println("Modulus: " + (num1 % num2)); // 3
+        System.out.println("Increment: " + (++num1)); // 16
+        System.out.println("Decrement: " + (--num2)); // 3
+        System.out.println("Post-Increment: " + (num1++)); // 16
+        System.out.println("Post-Decrement: " + (num2--)); // 3
+    }
+}
