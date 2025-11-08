161a191a87fe0418f81d08ec79f85f235b0935f6
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-09 02:31:30 +0530

    Create Demo20_Inheritence.java

---
+++ Core Java/Demo20/Demo20_Inheritence.java
@@ -0,0 +1,17 @@
+import Demo20_tools.*;   // * is used for files in the package not folders
+import java.lang.System;// java.lang package is imported by default
+public class Demo20_Inheritence {
+    public static void main(String[] args) {
+        // Multiple inheritance is not supported in Java using classes
+        // but can be achieved using interfaces.
+        AdvCalculator calc = new AdvCalculator();
+        int sum = calc.add(5, 3);
+        int difference = calc.subtract(10, 4);
+        int multiply = calc.multiply(2, 3);
+        double division = calc.divide(10, 2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Multiplication: " + multiply);
+        System.out.println("Division: " + division);
+    }
+}
