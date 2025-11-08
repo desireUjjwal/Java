5905c50e2aebb70f06055cc243abffc02dec33a1
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-08 22:13:59 +0530

    Update Demo17_Encapsulation.java

--- Core Java/Demo17_Encapsulation.java
+++ Core Java/Demo17_Encapsulation.java
@@ -4,8 +4,9 @@ class Human{
     public int getAge() {
         return age;
     }
-    public void setAge(int age) {
-        this.age = age;
+    public void setAge(int age) {   //(int age, Human obj))
+        // obj.age = age;
+        this.age = age;   // this keyword is used to refer to the current object's instance variable
     }
     public String getName() {
         return name;
@@ -18,6 +19,7 @@ public class Demo17_Encapsulation {
     public static void main(String[] args) {
         Human obj = new Human();
         obj.setAge(25);
+        // obj.setAge(25, obj); // If we won't use this then we need to pass the obj to change it
         obj.setName("Ujjwal");
         System.out.println("Name: " + obj.getName());
         System.out.println("Age: " + obj.getAge());
