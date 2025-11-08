a7dc79f7bd1854aeeade72071a61787fd04f85fb
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-09 00:03:18 +0530

    Create Demo23_AccessModifiers.java

---
+++ Core Java/Demo23_AccessModifiers.java
@@ -0,0 +1,9 @@
+public class Demo23_AccessModifiers {
+    public static void main(String[] args) {
+        // public can be accessed from anywhere
+        // private can be accessed only within the same class
+        // protected can be accessed within the same package and subclasses
+        // default (no modifier) can be accessed only within the same package
+        // We can't have to public classes in a single file
+    }
+}
