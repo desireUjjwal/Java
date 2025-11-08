47b59f9759d73a38e3b807a83196602011f048eb
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-09 02:50:32 +0530

    Update and rename Demo2_AbstractClass.java to Demo02_AbstractClass.java

---
+++ Advance Java/Demo02_AbstractClass.java
@@ -0,0 +1,22 @@
+
+abstract class Car{
+    public abstract void drive(); // Abstract method can only be declared in abstract class
+    public void playMusic(){  // Abstract class can have all concrete methods
+        System.out.println("Playing music");
+    }
+}
+
+class WagonR extends Car{
+    @Override
+    public void drive() {        // We need to provide implementation of abstract method
+        // If all abstract methods not implemented, then the subclass must be declared abstract
+        System.out.println("Driving WagonR");
+    }
+}
+public class Demo02_AbstractClass {
+    public static void main(String[] args) {
+        // Car myCar = new Car(); // Error: Cannot instantiate the abstract class
+        Car myCar = new WagonR(); // Upcasting
+        myCar.drive(); // Calls the implemented method in WagonR
+    }
+}
