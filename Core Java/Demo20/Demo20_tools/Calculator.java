826d2e850999da8e11665e8fc5bb2b3dc8d71cba
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-09 02:29:50 +0530

    Create Calculator.java

---
+++ Core Java/Demo20/Demo20_tools/Calculator.java
@@ -0,0 +1,11 @@
+package Demo20_tools;
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    
