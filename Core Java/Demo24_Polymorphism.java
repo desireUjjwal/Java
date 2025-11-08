db607b2b18584c9a347dbb666368d36aa9304968
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-09 00:13:35 +0530

    Create Demo24_Polymorphism.java

---
+++ Core Java/Demo24_Polymorphism.java
@@ -0,0 +1,8 @@
+public class Demo24_Polymorphism {
+    public static void main(String[] args) {
+        // Compile-time polymorphism (Method Overloading) (Early binding)
+        // Run-time polymorphism (Method Overriding) (Late binding)
+        // Overloading: add(int a, int b), add(double a, double b)
+        // Overriding: class A { void show() }, class B extends A { void show() }
+    }
+}
