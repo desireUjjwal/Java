64466e78e1a9e0a69fd9292a8175495243ff1c5f
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-11 06:18:07 +0530

    Create Demo18_ExceptionThrow.java

---
+++ Advance Java/Demo18_ExceptionThrow.java
@@ -0,0 +1,21 @@
+public class Demo18_ExceptionThrow {
+    public static void main(String[] args) {
+        int i = 20;
+        int j = 0;
+
+        try{
+            j = 16 / i;
+            if(j == 0){
+                throw new ArithmeticException("Value of j should not be more than 16");
+            }
+
+        }catch(ArithmeticException e){
+            j = 16 / 1;
+            System.out.println("That's the deafault output. " + e);
+        }catch(Exception e){ 
+            System.out.println("Something went wrong: " + e);
+        }
+
+        System.out.println("The value of j is: " + j); 
+    }
+}
