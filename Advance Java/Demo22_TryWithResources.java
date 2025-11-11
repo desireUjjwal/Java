0f5b2c1d208642f94a9e36726baa43f05cd0490a
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-11 19:43:54 +0530

    Create Demo22_TryWithResources.java

---
+++ Advance Java/Demo22_TryWithResources.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Demo22_TryWithResources {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int num = 0;
+        // BufferedReader br = null;
+        // try{
+        //     InputStreamReader in = new InputStreamReader(System.in);
+        //     br = new BufferedReader(in);
+        //     num = Integer.parseInt(br.readLine());
+        //     System.out.println(num);
+
+        // }finally{ // finally is used to close resources
+        //     br.close();
+        // }
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){ // try with resource
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+
+        }
+    }
+}
