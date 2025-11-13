2657f3db5d74cb5620221d9fe40b864bb09c980f
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-13 18:33:19 +0530

    Create Demo33_StreamApi.java

---
+++ Advance Java/Demo33_StreamApi.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Demo33_StreamApi {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(4,5,7,3,2,6);
+        // Stream<Integer> s1 = nums.stream();
+        // Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
+        // Stream<Integer> s3 = s2.map(n -> n*2);
+        // int result = s3.reduce(0, (c,e) -> c+e);
+        int result = nums.stream()
+                         .filter(n -> n % 2 == 0)
+                         .map(n -> n * 2)
+                         .reduce(0, (c,e) -> c+e);
+        System.out.println(result);
+        
+    }
+}
