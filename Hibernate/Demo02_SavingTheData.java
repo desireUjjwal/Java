3dad9a85259329c5d311cf034c06332ad082c9ab
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-14 18:49:42 +0530

    Create Demo02_SavingTheData.java

---
+++ Hibernate/Demo02_SavingTheData.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Demo02_SavingTheData {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setsName("Avni");
+        s1.setRollNo(6);
+        s1.setsAge(25);
+
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(org.example.Student.class);
+//        cfg.configure();
+//        SessionFactory sf = cfg.buildSessionFactory();
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.example.Student.class)
+                .configure()
+                .buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+        session.persist(s1);
+        transaction.commit();
+        session.close();
+        sf.close();
+    }
+}
