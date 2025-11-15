3df94d8741aa782ad441d15109e3b66c02022b73
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-15 17:59:49 +0530

    Update Demo02_SavingTheData.java

--- Hibernate/Demo02_SavingTheData.java
+++ Hibernate/Demo02_SavingTheData.java
@@ -5,8 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Demo02_SavingTheData {
     public static void main(String[] args) {
         Student s1 = new Student();
