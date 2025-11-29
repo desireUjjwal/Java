ac72b126933642483d70252dd44913065c1496ad
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-30 03:36:31 +0530

    Implement Many-to-Many relationship example

---
+++ Hibernate/Demo11_ManyToMany.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Laptop l1 = new Laptop();
+        l1.setLid(1);
+        l1.setBrand("Lenovo");
+        l1.setModel("Legion");
+        l1.setRam(8);
+
+        Laptop l2 = new Laptop();
+        l2.setLid(2);
+        l2.setBrand("Dell");
+        l2.setModel("XPS");
+        l2.setRam(32);
+
+        Laptop l3 = new Laptop();
+        l3.setLid(3);
+        l3.setBrand("Apple");
+        l3.setModel("Macbook");
+        l3.setRam(8);
+
+        Alien a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("Shiva");
+        a1.setTech("SpringBoot");
+
+
+        Alien a2 = new Alien();
+        a2.setAid(102);
+        a2.setAname("Harsh");
+        a2.setTech("Python");
+
+        Alien a3 = new Alien();
+        a3.setAid(103);
+        a3.setAname("Kiran");
+        a3.setTech("Ai");
+
+        a1.setLaptops(Arrays.asList(l1,l2));
+        a2.setLaptops(Arrays.asList(l2,l3));
+        a3.setLaptops(Arrays.asList(l1));
+
+        l1.setAliens(Arrays.asList(a1,a3));
+        l2.setAliens(Arrays.asList(a1,a2));
+        l3.setAliens(Arrays.asList(a2));
+
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.example.Alien.class)
+                .addAnnotatedClass(org.example.Laptop.class)
+                .configure()
+                .buildSessionFactory();
+
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(l1); // It needs to be persisted first because laptop is the primary table now
+        session.persist(l2);
+        session.persist(l3);
+
+        session.persist(a1);
+        session.persist(a2);
+        session.persist(a3);
+
+        transaction.commit();
+        Alien a5 = session.get(Alien.class, 102); // why this is not firing a select query
+        System.out.println(a5);
+
+        session.close();
+        sf.close();
+    }
+}
