2f5ef6a4436dca88f7c6beb530d2819ca754dbe9
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-30 04:05:11 +0530

    Create Demo12_Alien.java

---
+++ Hibernate/Demo12_Alien.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+
+//@Entity(name="alien_table")
+@Entity
+public class Alien {
+    @Id
+    private  int aid;
+    private String aname;
+    private String tech;
+    @OneToMany(fetch = FetchType.EAGER ) // By default fetch type is LAZY
+    private List<Laptop> Laptops;
+
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+
+    public List<Laptop> getLaptops() {
+        return Laptops;
+    }
+
+    public void setLaptops(List<Laptop> laptops) {
+        Laptops = laptops;
+    }
+
+    @Override
+    public String toString() {
+        return "Alien{" +
+                "aid=" + aid +
+                ", aname='" + aname + '\'' +
+                ", tech='" + tech + '\'' +
+                ", Laptops=" + Laptops +
+                '}';
+    }
+}
