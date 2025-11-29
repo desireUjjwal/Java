3e5fff9d2b9c1a59aef8370daafb18b5458810be
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-30 03:36:58 +0530

    Create Demo11_Laptop.java

---
+++ Hibernate/Demo11_Laptop.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Laptop {
+    @Id
+    public int lid;
+    private String brand;
+    private  String model;
+    private int ram;
+    @ManyToMany(mappedBy = "Laptops")
+    private List<Alien> aliens;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public List<Alien> getAliens() {
+        return aliens;
+    }
+
+    public void setAliens(List<Alien> aliens) {
+        this.aliens = aliens;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "lid=" + lid +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", ram=" + ram +
+                '}';
+    }
+}
