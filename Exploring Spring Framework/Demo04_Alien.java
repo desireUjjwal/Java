13f26ea7b911c8fb47e1666c6057c1a305acff29
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-30 21:41:19 +0530

    Add Alien class with age property and methods

---
+++ Exploring Spring Framework/Demo04_Alien.java
@@ -0,0 +1,21 @@
+package com.ujjwal;
+
+public class Alien {
+    int age;
+    public Alien(){
+        System.out.println("Alien Object Created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter Called");
+        this.age = age;
+    }
+
+    public void code(){
+        System.out.println("Coding...");
+    }
+}
