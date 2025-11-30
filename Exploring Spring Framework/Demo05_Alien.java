85d0b1937f352f5b89d6a6816e73e29c8a06ff73
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-30 22:02:39 +0530

    Create Demo05_Alien.java

---
+++ Exploring Spring Framework/Demo05_Alien.java
@@ -0,0 +1,31 @@
+package com.ujjwal;
+
+public class Alien {
+    int age;
+    private Laptop lap;
+    public Alien(){
+        System.out.println("Alien Object Created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter Called");
+        this.age = age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+    public void code(){
+        System.out.println("Coding...");
+        lap.compile();
+    }
+}
