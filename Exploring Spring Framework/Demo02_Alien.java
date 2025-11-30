ec6c24a5341c7fe57236c1986b0c63055c8e850d
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-30 21:19:17 +0530

    Create Demo02_Alien.java

---
+++ Exploring Spring Framework/Demo02_Alien.java
@@ -0,0 +1,10 @@
+package com.ujjwal;
+
+public class Alien {
+    public Alien(){
+        System.out.println("Alien Object Created");
+    }
+    public void code(){
+        System.out.println("Coding...");
+    }
+}
