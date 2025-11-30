741a2eb3461eddd93786819981a0156e2ddcd342
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-11-30 14:51:58 +0530

    Create Demo01_DependencyInjectionUsingSpringBoot.java

---
+++ JDBC/SpringGettingStarted/Demo01_DependencyInjectionUsingSpringBoot.java
@@ -0,0 +1,20 @@
+package com.ujjwal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context=  SpringApplication.run(SpringBootDemoApplication.class, args);
+
+        Alien obj = context.getBean(Alien.class);
+        obj.code();
+
+        Alien obj1 = context.getBean(Alien.class); // are we getting same obj or diffrent obj ?
+        obj1.code();
+
+	}
+}
