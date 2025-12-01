5a7d83327447999e6f892ba73de3f39edb702403
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-01 18:37:58 +0530

    Create Demo01_config_AppConfig.java

---
+++ Spring Java Based Configuration/Demo01_config_AppConfig.java
@@ -0,0 +1,13 @@
+package com.ujjwal.config;
+
+import com.ujjwal.Desktop;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Desktop desktop(){
+        return new Desktop();
+    }
+}
