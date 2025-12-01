b5014cccfc9c78b9ec7761e18784b40b27e2e1d9
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-01 22:11:27 +0530

    Create Demo07_Laptop.java

---
+++ Spring Java Based Configuration/Demo07_Laptop.java
@@ -0,0 +1,15 @@
+package com.ujjwal;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop implements Computer {
+    public Laptop(){
+        System.out.println("Laptop Object Created");
+    }
+
+    @Override
+    public void compile(){
+        System.out.println("Compiling using laptop");
+    }
+}
