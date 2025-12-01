55a7b959bcc92900557a2721592bdf61862c2429
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-01 20:49:02 +0530

    Create Demo06_config_AppConfig.java

---
+++ Spring Java Based Configuration/Demo06_config_AppConfig.java
@@ -0,0 +1,32 @@
+package com.ujjwal.config;
+
+import com.ujjwal.Alien;
+import com.ujjwal.Computer;
+import com.ujjwal.Desktop;
+import com.ujjwal.Laptop;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan("com.ujjwal")
+public class AppConfig {
+//    @Bean
+//    public Alien alien(Computer com){ // @Autowired is optional @Qualifier("desktop")
+//        Alien obj = new Alien();
+//        obj.setAge(25);
+//        obj.setCom(com);
+//        return obj;
+//    }
+//
+//    @Bean
+////    @Scope("prototype")
+//    public Desktop desktop(){
+//        return new Desktop();
+//    }
+//    @Bean
+//    @Primary
+//    public Laptop laptop(){
+//        return new Laptop();
+//    }
+}
