20772c13bea094c6364e8d0c02dad77311ba694e
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-01 20:49:27 +0530

    Create Demo06_Alien.java

---
+++ Spring Java Based Configuration/Demo06_Alien.java
@@ -0,0 +1,35 @@
+package com.ujjwal;
+
+import org.springframework.stereotype.Component;
+
+import java.beans.ConstructorProperties;
+@Component
+public class Alien {
+    private int age;
+    private Computer com;
+    public Alien(){
+        System.out.println("Alien Object Created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter Called");
+        this.age = age;
+    }
+
+    public Computer getCom() {
+        return com;
+    }
+
+    public void setCom(Computer com) {
+        this.com = com;
+    }
+
+    public void code(){
+        System.out.println("Coding...");
+        com.compile();
+    }
+}
