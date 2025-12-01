19510be6f3db1f91e3794e07562a37670893eef9
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-01 22:33:52 +0530

    Create Demo09_Alien.java

---
+++ Spring Java Based Configuration/Demo09_Alien.java
@@ -0,0 +1,42 @@
+package com.ujjwal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.beans.ConstructorProperties;
+@Component
+public class Alien {
+    @Value("21")
+    private int age;
+//    @Autowired //--> Field Injection // Injection can be done in 3 levels --> 1.field injection(By default)  2.Constructor Injection 3.Setter Injection
+//    @Qualifier("com2") // By default, bean name is class name but first letter should be small(eg: laptop, desktop)
+    private Computer com;
+    public Alien(){
+        System.out.println("Alien Object Created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter Called");
+        this.age = age;
+    }
+
+    public Computer getCom() {
+        return com;
+    }
+    @Autowired
+    @Qualifier("laptop") // If we have both @Qualifier and @Primary @Qualifier will be preferred
+    public void setCom(Computer com) {
+        this.com = com;
+    }
+
+    public void code(){
+        System.out.println("Coding...");
+        com.compile();
+    }
+}
