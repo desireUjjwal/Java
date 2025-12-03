3302ba59562df0f6f05991a1fb0c78cd3fa45d7d
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-03 09:31:42 +0530

    Create Demo02_Laptop.java

---
+++ Moving To SpringBoot/Demo02_Laptop.java
@@ -0,0 +1,11 @@
+package com.ujjwal.app;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop  implements Computer {
+    public void compile(){
+        System.out.println("Compiling using Laptop");
+    }
+}
