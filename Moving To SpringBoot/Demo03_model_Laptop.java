e4857ae3397400ef9135cdc5e92fb3a0c33ab319
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-03 13:02:28 +0530

    Create Demo03_model_Laptop.java

---
+++ Moving To SpringBoot/Demo03_model_Laptop.java
@@ -0,0 +1,10 @@
+package com.ujjwal.app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop  implements Computer {
+    public void compile(){
+        System.out.println("Compiling using Laptop");
+    }
+}
