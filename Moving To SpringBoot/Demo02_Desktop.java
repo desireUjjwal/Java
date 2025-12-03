452a86a927fc5a3514208e10da0796a87adb3f0c
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-03 09:32:11 +0530

    Create Demo02_Desktop.java

---
+++ Moving To SpringBoot/Demo02_Desktop.java
@@ -0,0 +1,13 @@
+package com.ujjwal.app;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class Desktop implements Computer{
+    @Override
+    public void compile() {
+        System.out.println("Compiling using Desktop");
+    }
+}
