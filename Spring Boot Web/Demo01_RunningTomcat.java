f62ab3244f9db837fe523eb4a1b9c12889433080
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-04 18:10:32 +0530

    Create Demo01_RunningTomcat.java

---
+++ Spring Boot Web/Demo01_RunningTomcat.java
@@ -0,0 +1,14 @@
+package com.ujjwal;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App
+{
+    public static void main( String[] args ) throws LifecycleException {
+        System.out.println( "Hello World!" );
+        Tomcat tomcat = new Tomcat();
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
