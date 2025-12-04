b9dd0bff2a5520f9de5cd5d64ebf0a69905d9a50
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-04 10:39:45 +0530

    Create Demo01_repo_StudentRepo.java

---
+++ Spring JDBC/Demo01_repo_StudentRepo.java
@@ -0,0 +1,19 @@
+package com.ujjwal.SpringJDBCEx.repo;
+
+import com.ujjwal.SpringJDBCEx.model.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class StudentRepo {
+    public void save(Student s) {
+        System.out.println("Added");
+    }
+
+    public List<Student> findAll() {
+        List<Student> students = new ArrayList<>();
+        return students;
+    }
+}
