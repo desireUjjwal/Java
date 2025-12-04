039da7ce4e32c8b24b192028cca0b90631a200c5
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-04 11:10:09 +0530

    Create Demo02_resources_data.sql

---
+++ Spring JDBC/Demo02_resources_data.sql
@@ -0,0 +1,3 @@
+insert into student(rollno, name, marks) values (101,'Kiran', 79);
+insert into student(rollno, name, marks) values (102,'Harsh', 69);
+insert into student(rollno, name, marks) values (103,'Sushil', 89);
