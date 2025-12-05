fb1c3aabf99440688ee4ff3e2152f8f4c5be543b
desireUjjwal <78691475+desireUjjwal@users.noreply.github.com>
2025-12-05 15:58:34 +0530

    Create Demo15_HomeController.java

---
+++ Spring Boot Web/Demo15_HomeController.java
@@ -0,0 +1,38 @@
+package com.ujjwal.SpringBootWeb1;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller //Stereotype Annotation
+public class HomeController {
+    @RequestMapping("/")   //@GetMapping, @PostMapping, @PutMapping, @DeleteMapping
+    public String home(){
+        System.out.println("Home method called");
+        return "index";
+    }
+
+
+    @RequestMapping("add")
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){
+
+        int result = num1 + num2;
+        mv.addObject("result", result);
+        mv.setViewName("result");
+        return mv;
+    }
+
+    @RequestMapping("addAlien")
+    public ModelAndView addAlien(@RequestParam("aid") int aid, @RequestParam("aname") String aname, ModelAndView mv){
+
+        Alien alien = new Alien();
+        alien.setAid(aid);
+        alien.setAname(aname);
+        mv.addObject("alien", alien);
+        mv.setViewName("result");
+        return mv;
+    }
+}
